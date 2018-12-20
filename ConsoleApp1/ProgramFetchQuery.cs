f3c4d1e838ec183e948c71845f88a20785213210
Yuhoo <vicky.huangyuanxin@gmail.com>
2018-12-20 16:03:56 +0100

    Create ProgramFetchQuery.cs
    Add program fetch query

---
+++ ConsoleApp1/ProgramFetchQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+
+
+namespace ProgramFetchQuery
+{
+    class ProgramFetchQuery
+    {
+        
+
+    }
+}
