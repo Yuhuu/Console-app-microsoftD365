7a8a24effc2226674d77cbf7e2012002972bb9a3
Yuhoo <vicky.huang@sectoralarm.com>
2019-02-22 13:36:07 +0100

    Add to a method to check timezone
    check timezone

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.ServiceModel.Description;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Xrm.Sdk.Query;
@@ -12,17 +11,17 @@ using Microsoft.Xrm.Sdk.Query;
 
 namespace LeadProcess
 {
- /*
- * Lead is a protential buyer. Lead Process is a class to show the customer acquisition process which
- * ldentifies potential buyers. And when the leads are considered qualified, get passed from marketing to create 
- * more entities. The purpose of it is to effective lead management is ultimately contribute to more sales* 
- */
+    /*
+    * Lead is a protential buyer. Lead Process is a class to show the customer acquisition process which
+    * ldentifies potential buyers. And when the leads are considered qualified, get passed from marketing to create 
+    * more entities. The purpose of it is to effective lead management is ultimately contribute to more sales* 
+    */
     class LeadProcess
     {
-        
+
         private const string currency = "NOK";
-        private const string leadSourceName = "Autolease";
-        public const string companyName = "Alexander Angelov";
+        private const string leadSourceName = "Test";
+        public const string companyName = "Abas Abdullahi Ali";
         //public const string companyName = "5th week AS"; 
 
         public static void Main(string[] args)
@@ -33,10 +32,41 @@ namespace LeadProcess
             var clientCredentials = new ClientCredentials();
             clientCredentials.UserName.UserName = credentials.Domain + "\\" + credentials.UserName;
             clientCredentials.UserName.Password = credentials.Password;
-            var service = new OrganizationServiceProxy(new Uri("http://dynamicscrmproxysvctest/CrmSMSOutboundProxy.svc"), null, clientCredentials, null);
+            //playground
+            // var service = new OrganizationServiceProxy(new Uri("https://intmscrmtst.sectoralarm.net/SectorAlarmfrtstPLAYGROUND/XRMServices/2011/Organization.svc"), null, clientCredentials, null);
+            //DEV
+            var organizationList = new List<IOrganizationService>();
+            var countryList = new List<string>() { "NO", "SE", "FI", "ES", "IE", "FR" };
+            foreach (var country in countryList)
+            {
+                var addService = new OrganizationServiceProxy(new Uri("https://" + $"intmscrmtst.sectoralarm.net/SectorAlarm{country}tst/XRMServices/2011/Organization.svc"), null, clientCredentials, null);
+                organizationList.Add(addService);
+            }
+            var service = new OrganizationServiceProxy(new Uri("http://sanocrm16d01/SANO/XRMServices/2011/Organization.svc"), null, clientCredentials, null);
+            //var qAccount = new QueryExpression("account");
+            //qAccount.Criteria.AddCondition("adsasdf", ConditionOperator.Equal, "something");
+            //var getAccount = service.RetrieveMultiple(qAccount).Entities;
+            //var thisAccount = getAccount.First();
 
-            Guid id = CreateLeadWithName(service, companyName);
+            var alocalZone = TimeZoneInfo.Local;
+            var throwALl = new StringBuilder();
+            //throwALl.AppendLine($"Local:{alocalZone}");
+            var throwThis = TimeZoneInfo.GetSystemTimeZones();
+            //throwALl.AppendLine($"'My' system: {TimeZoneInfo.GetSystemTimeZones();}");
+
+            foreach (var org in organizationList)
+            {
+                var alocalZone1 = TimeZoneInfo.Local;
+                var throwALl1 = new StringBuilder();
+                throwALl.AppendLine($"Local:{alocalZone1}");
+            }
+
+            throw new Exception(throwALl.ToString());
+
+           Guid id = CreateLeadWithName(service, companyName);
             //CreateSMSWithName(service, companyName);
+            //QualifyLead(service, id);
+            //Guid id = CreateLeadWithSimpleinfor(service, companyName);
             QualifyLeadWithMovingOut(service, id);
             //UpdateLead(service, id);
             //QualifyLead(service, id);
@@ -72,7 +102,8 @@ namespace LeadProcess
             return resultLise.Entities.FirstOrDefault();
 
         }
-        private static Entity GetUser( OrganizationServiceProxy service) {
+        private static Entity GetUser(OrganizationServiceProxy service)
+        {
             var query = new QueryExpression("systemuser");
             query.Criteria.AddCondition("domainname", ConditionOperator.Equal, @"SECTORALARM\sayahuan");
             var resultLise = service.RetrieveMultiple(query);
@@ -123,7 +154,6 @@ namespace LeadProcess
             return post;
 
         }
-
         private static Entity GetTakdOverCASE(OrganizationServiceProxy service)
         {
             var query = new QueryExpression("incident");
@@ -157,7 +187,7 @@ namespace LeadProcess
         private static Entity GetInstallation(OrganizationServiceProxy service)
         {
             var query = new QueryExpression("log_installation");
-            query.Criteria.AddCondition("log_hardwareid", ConditionOperator.Equal, "New Installation: Navn Etternavn");
+            query.Criteria.AddCondition("log_alarmsystemid", ConditionOperator.Equal, "installationid");
             var resultLise = service.RetrieveMultiple(query);
 
             if (resultLise.Entities.Count == 0)
@@ -194,8 +224,53 @@ namespace LeadProcess
             newLead["companyname"] = name;
             newLead["log_leadsourceid"] = leadSource.ToEntityReference();
             //newLead["ownerid"] = user.ToEntityReference();
-            newLead["log_newaccount"] = GetAccount(service).ToEntityReference();
             newLead["log_direction"] = new OptionSetValue(182400001); //moved out
+            newLead["log_contracttermsid"] = GetContractTerms(service).ToEntityReference();
+            newLead["telephone2"] = "45512131";
+            newLead["log_dateofbirth"] = DateTime.Now.AddYears(-19);
+            //Date cannot be in the future
+            newLead["log_solddate"] = DateTime.Now;
+            newLead["log_movingdate"] = DateTime.Now;
+
+            newLead["log_salespersonid"] = GetSalesPerson(service).ToEntityReference();
+            //street 1
+            //newLead["address2_line1"] = "address2 vitaminveien 1, oslo";
+            newLead["log_address2_postalcode"] = GetPostCode(service).ToEntityReference();
+            newLead["log_postalcode"] = GetPostCode(service).ToEntityReference();
+            newLead["address1_line1"] = "address1 vitaminveien 1, oslo";
+            newLead["log_canoverwritecreditcheck"] = true;
+            newLead["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
+            newLead["log_typeoflead"] = new OptionSetValue(182400002);
+            newLead["log_movefrominstallation"] = GetInstallation(service).ToEntityReference();
+            newLead["log_movetoinstallation"] = GetInstallation(service).ToEntityReference();
+            return service.Create(newLead);
+        }
+        /*
+        *  Lead create Process allow end user to choose which name they want to use 
+        *  The purpose of we want the name as a parameter is that it can be easily track and create.
+        */
+        private static Guid CreateLeadWithSimpleinfor(OrganizationServiceProxy service, String name)
+        {
+
+            var leadSource = GetLeadSourceName(service, leadSourceName);
+            var newLead = new Entity("lead");
+
+            var user = GetUser(service);
+            newLead["companyname"] = name;
+            newLead["log_leadsourceid"] = leadSource.ToEntityReference();
+            //newLead["ownerid"] = user.ToEntityReference();
+            newLead["log_direction"] = new OptionSetValue(182400001); //moved out
+            newLead["log_contracttermsid"] = GetContractTerms(service).ToEntityReference();
+
+            newLead["log_dateofbirth"] = DateTime.Now.AddYears(-19);
+            //Date cannot be in the future
+            newLead["log_solddate"] = DateTime.Now;
+            newLead["log_movingdate"] = DateTime.Now;
+
+            newLead["log_salespersonid"] = GetSalesPerson(service).ToEntityReference();
+            //street 1
+            newLead["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
+            newLead["log_typeoflead"] = new OptionSetValue(182400002);
             return service.Create(newLead);
         }
 
@@ -242,13 +317,19 @@ namespace LeadProcess
         */
         private static void QualifyLead(OrganizationServiceProxy service, Guid id)
         {
+            var createEntity = new Entity("log_workorderproduct");
+            createEntity["log_leadid"] = new EntityReference("lead", id);
+            service.Create(createEntity);
+
             var entity = new Entity("lead");
             entity["leadid"] = id;
-            entity["log_contracttermsid"] = GetContractTerms(service).ToEntityReference();
-            entity["log_dateofbirth"] = DateTime.Now.AddYears(-19);
-            //Date cannot be in the future
-            entity["log_solddate"] = DateTime.Now;
+            //entity["log_contracttermsid"] = GetContractTerms(service).ToEntityReference();
+
+            //entity["log_dateofbirth"] = DateTime.Now.AddYears(-19);
+            ////Date cannot be in the future
+            //entity["log_solddate"] = DateTime.Now;
             entity["log_movingdate"] = DateTime.Now;
+            service.Update(entity);
 
             entity["log_salespersonid"] = GetSalesPerson(service).ToEntityReference();
             //street 1
@@ -257,14 +338,15 @@ namespace LeadProcess
             entity["log_postalcode"] = GetPostCode(service).ToEntityReference();
             entity["address1_line1"] = "address1 vitaminveien 1, oslo";
             entity["log_canoverwritecreditcheck"] = true;
+            entity["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
+            //entity["log_typeofcoverage"] = new OptionSetValue(284390001);
+            //entity["log_typeoflead"] = new OptionSetValue(182400002);
             service.Update(entity);
 
-            var createEntity = new Entity("log_workorderproduct");
-                createEntity["log_leadid"] = new EntityReference("lead", id);
-                    service.Create(createEntity);
+            //var createEntity = new Entity("log_workorderproduct");
+            //createEntity["log_leadid"] = new EntityReference("lead", id);
+            //service.Create(createEntity);
 
-         
-            entity["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
             entity["log_convertleadflag"] = 1;
 
             service.Update(entity);
@@ -289,27 +371,28 @@ namespace LeadProcess
             entity["log_address2_postalcode"] = GetPostCode(service).ToEntityReference();
             entity["log_postalcode"] = GetPostCode(service).ToEntityReference();
             entity["address1_line1"] = "address1 vitaminveien 1, oslo";
+
+            //entity["address2_line1"] = "address2 vitaminveien 1, oslo";
             entity["log_canoverwritecreditcheck"] = true;
-   
 
             var createEntity = new Entity("log_workorderproduct");
             createEntity["log_leadid"] = new EntityReference("lead", id);
             service.Create(createEntity);
 
-
-            entity["address2_line1"] = "address2 vitaminveien 1, oslo";
             entity["log_direction"] = new OptionSetValue(182400001);
             entity["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
+            entity["log_convertleadflag"] = 1;
+            entity["log_typeoflead"] = new OptionSetValue(182400002);
+            entity["log_movefrominstallation"] = GetInstallation(service).ToEntityReference();
             //moved out
-            service.Update(entity);
+            //service.Update(entity);
 
-            entity["log_takeovercase"] = GetTakdOverCASE(service).ToEntityReference();
             entity["log_convertleadflag"] = 1;
 
             //entity["statecode"] = new OptionSetValue(1);
             //entity["statuscode"] = new OptionSetValue(3);
 
-            //service.Update(entity);
+            service.Update(entity);
         }
     }
 }
